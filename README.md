8d1dcdc0065150890711bc8f475eaf3021eeb4a2
ZxBing0066 <ZxBing0066@gmail.com>
2017-07-23 00:31:57 +0800

    add codecov & some test case

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # JOB RUNNER
 
 [![Build Status](https://travis-ci.org/ZxBing0066/node-job-runner.svg?branch=master)](https://travis-ci.org/ZxBing0066/node-job-runner)
+
+[![codecov](https://codecov.io/gh/ZxBing0066/node-job-runner/branch/master/graph/badge.svg)](https://codecov.io/gh/ZxBing0066/node-job-runner)
--- test/runner.test.js
+++ test/runner.test.js
@@ -1,14 +1,14 @@
 let chai = require('chai');
 chai.use(require("chai-as-promised"));
 let assert = require('assert');
-let expect = require('chai').expect;
 
-let Runner = require('../index').default
+let jobRunner = require('../index')
+let Runner = jobRunner.default;
 let source = require('./source')
 let sourceData = source.map(d => d.data)
 
-describe('Runner group', function () {
-    this.timeout(1000 * 60 * 5);
+describe('Runner group test', function () {
+    // this.timeout(1000 * 60 * 5);
 
     let result;
     let order = [];
@@ -36,19 +36,19 @@ describe('Runner group', function () {
         assert.equal(result.length, source.length)
     })
 
-    it('shoult the result equal to source data', () => {
+    it('should the result equal to source data', () => {
         console.log('result: ', result)
         assert.deepEqual(result, sourceData)
     })
 
-    it('shoult block the queue when some runner blocked', () => {
+    it('should block the queue when some runner blocked', () => {
         console.log('order: ', order)
         assert.equal(order[1], 0)
     })
 
 })
 
-describe('Runner without group', function () {
+describe('Runner without group test', function () {
     this.timeout(1000 * 60 * 5);
 
     let result;
@@ -76,14 +76,87 @@ describe('Runner without group', function () {
         assert.equal(result.length, source.length)
     })
 
-    it('shoult the result equal to source data', () => {
+    it('should the result equal to source data', () => {
         console.log('result: ', result)
         assert.deepEqual(result, sourceData)
     })
 
-    it('shoult not block the queue when some runner blocked', () => {
+    it('should not block the queue when some runner blocked', () => {
         console.log('order: ', order)
         assert.equal(order[order.length - 1], 0)
+    })
+
+})
+
+describe('Create test', function () {
+    this.timeout(1000 * 60 * 5);
+
+    let result;
+    let order = [];
+
+    let runner = jobRunner.create({
+        source,
+        step: 2,
+        runner: (sourceData, index) => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(sourceData)
+                    order.push(sourceData.data)
+                    console.log(`do ${sourceData.data} now, source index: ${index}`)
+                }, sourceData.time)
+            })
+        }
+    })
+
+    before(() => {
+        return runner.run().then(res => result = res.map(d => d.data))
+    })
+
+    it('should result length equal to source length', () => {
+        assert.equal(result.length, source.length)
+    })
+
+    it('should the result equal to source data', () => {
+        console.log('result: ', result)
+        assert.deepEqual(result, sourceData)
+    })
+
+    it('should not block the queue when some runner blocked', () => {
+        console.log('order: ', order)
+        assert.equal(order[order.length - 1], 0)
+    })
+
+})
+
+
+describe('SetOption test', function () {
+    this.timeout(1000 * 60 * 5);
+
+    let result;
+    let order = [];
+
+    let runner = jobRunner.create({
+        source,
+        step: 2,
+        runner: (sourceData, index) => {
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    resolve(sourceData)
+                    order.push(sourceData.data)
+                    console.log(`do ${sourceData.data} now, source index: ${index}`)
+                }, sourceData.time)
+            })
+        }
+    })
+
+    it('should options update', () => {
+        let options = runner.options;
+        assert.equal(options.step, 2)
+        runner.setOptions({ step: 3 })
+        assert.equal(options.step, 3)
+        assert.equal(options.group, undefined)
+        runner.setOptions({ group: true })
+        assert.equal(options.group, true)
     })
 
 })
